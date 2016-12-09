d4c4254dc9a70b4174459ea27c83e41936727edd
YaR <yar229@yandex.ru>
2016-12-10 01:17:20 +0300

    custom Path utility class

---
+++ WebDavMailRuCloudStore/WebDAVPath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaR.WebDavMailRu.CloudStore
+{
+    public static class WebDAVPath
+    {
+        public static string Combine(string a, string b)
+        {
+            a = Clean(a);
+            b = Clean(b);
+            a = a.Trim('/');
+            b = b.TrimStart('/');
+            string res = "/" + a + (string.IsNullOrEmpty(b) ? "" : "/" + b);
+            return res;
+
+        }
+
+        public static string Clean(string path)
+        {
+            return path.Replace("\\", "/");
+        }
+
+    }
+}
