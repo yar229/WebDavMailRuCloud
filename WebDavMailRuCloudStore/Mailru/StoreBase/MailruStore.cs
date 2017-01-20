2b35bd52c28de6cd9d62685c8627572fad352dab
YaR <yar229@yandex.ru>
2017-01-21 02:45:46 +0300

    fix 404

--- WebDavMailRuCloudStore/Mailru/StoreBase/MailruStore.cs
+++ WebDavMailRuCloudStore/Mailru/StoreBase/MailruStore.cs
@@ -30,14 +30,31 @@ namespace YaR.WebDavMailRu.CloudStore.Mailru.StoreBase
             var path = GetPathFromUri(uri);
             
             //TODO: clean this trash
-            Entry item = null;
             try
             {
-                item = Cloud.Instance(httpContext).GetItems(path).Result;
-            }
-            catch (WebException e)
-            {
-                if (e.Status != WebExceptionStatus.ProtocolError) throw;
+                var item = Cloud.Instance(httpContext).GetItems(path).Result;
+                if (item != null)
+                {
+                    if (item.FullPath == path)
+                    {
+                        var dir = new Folder(path);
+                        return Task.FromResult<IStoreItem>(new MailruStoreCollection(httpContext, LockingManager, dir, IsWritable));
+                    }
+                    var fa = item.Files.FirstOrDefault(k => k.FullPath == path);
+                    if (fa != null)
+                        return Task.FromResult<IStoreItem>(new MailruStoreItem(LockingManager, fa, IsWritable));
+
+                    string parentPath = WebDavPath.Parent(path);
+                    item = Cloud.Instance(httpContext).GetItems(parentPath).Result;
+                    if (item != null)
+                    {
+                        var f = item.Files.FirstOrDefault(k => k.FullPath == path);
+                        return null != f
+                            ? Task.FromResult<IStoreItem>(new MailruStoreItem(LockingManager, f, IsWritable))
+                            : null;
+                    }
+
+                }
             }
             catch (AggregateException e)
             {
@@ -45,17 +62,7 @@ namespace YaR.WebDavMailRu.CloudStore.Mailru.StoreBase
                 if (we == null || we.Status != WebExceptionStatus.ProtocolError) throw;
             }
 
-            if (item?.FullPath == path)
-            {
-                var dir = new Folder(path);
-                return Task.FromResult<IStoreItem>(new MailruStoreCollection(httpContext, LockingManager, dir, IsWritable));
-            }
-
-            var f = item?.Files?.FirstOrDefault(k => k.FullPath == path);
-
-            if (null == f)
-                throw new FileNotFoundException();
-            return Task.FromResult<IStoreItem>(new MailruStoreItem(LockingManager, f, IsWritable));
+            return Task.FromResult<IStoreItem>(null);
         }
 
         public Task<IStoreCollection> GetCollectionAsync(Uri uri, IHttpContext httpContext)
