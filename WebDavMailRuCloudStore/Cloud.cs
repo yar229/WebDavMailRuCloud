386b7ea7226df02486a4a6adc50863dbe2caf88b
YaR <yar229@yandex.ru>
2017-02-08 17:18:59 +0300

    update nwebdav packages

--- WebDavMailRuCloudStore/Cloud.cs
+++ WebDavMailRuCloudStore/Cloud.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Net;
 using MailRuCloudApi;
 using MailRuCloudApi.Api;
