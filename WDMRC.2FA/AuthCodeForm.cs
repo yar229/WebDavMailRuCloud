9b3474a63a4c75ad3d390eadf78f676f411689f7
YaR <yar229@yandex.ru>
2017-06-01 10:29:46 +0300

    #77 2F auth form

---
+++ WDMRC.2FA/AuthCodeForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailRuCloudApi.TwoFA
+{
+    public partial class AuthCodeForm : Form
+    {
+        public AuthCodeForm()
+        {
+            InitializeComponent();
+        }
+
+        public AuthDialogResult ShowAuthDialog(string login, bool isRelogin)
+        {
+            txtLogin.Text = login;
+            lblInfo.Text = isRelogin
+                ? "Login request"
+                : "Auto relogin request";
+            txtAuthCode.Focus();
+
+            var res = ShowDialog();
+
+            return new AuthDialogResult
+            {
+                DialogResult = res,
+                AuthCode = txtAuthCode.Text
+            };
+        }
+
+    }
+
+    public class AuthDialogResult
+    {
+        public DialogResult DialogResult { get; set; }
+        public string AuthCode { get; set; }
+    }
+}
--- WDMRC.2FA/AuthCodeWindow.cs
+++ WDMRC.2FA/AuthCodeWindow.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,24 +16,51 @@ namespace MailRuCloudApi.TwoFA
 
         public string Get(string login, bool isAutoRelogin)
         {
-            Form prompt = new Form
-            {
-                Width = 500,
-                Height = 150,
-                FormBorderStyle = FormBorderStyle.FixedDialog,
-                Text = "WebDAVMailRuCloud 2-factor authentification",
-                StartPosition = FormStartPosition.CenterScreen
-            };
-            Label textLabel = new Label() { Left = 50, Top = 20, Text = isAutoRelogin ? "Auto relogin " : "Login " + login };
-            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
-            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
-            confirmation.Click += (sender, e) => { prompt.Close(); };
-            prompt.Controls.Add(textBox);
-            prompt.Controls.Add(confirmation);
-            prompt.Controls.Add(textLabel);
-            prompt.AcceptButton = confirmation;
+            string result = string.Empty;
 
-            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+            Application.EnableVisualStyles();
+
+            using (NotifyIcon notify = new NotifyIcon())
+            using (AuthCodeForm prompt = new AuthCodeForm())
+            {
+                notify.Visible = true;
+                notify.Icon = SystemIcons.Exclamation;
+                notify.BalloonTipTitle = "WebDAV Mail.Ru 2 Factor Auth";
+                notify.BalloonTipText = $"Auth code required for {login}";
+                notify.BalloonTipIcon = ToolTipIcon.Info;
+
+                notify.ShowBalloonTip(30000);
+
+                var res = prompt.ShowAuthDialog(login, isAutoRelogin);
+
+                return res.AuthCode;
+
+                //CancellationTokenSource cancelWait = new CancellationTokenSource();
+                //notify.Click += (o, eventArgs) =>
+                //{
+                //    result = prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+                //    cancelWait.Cancel();
+                //};
+
+                //notify.ShowBalloonTip(30000);
+
+                //var t =  new Task(() =>
+                //{
+                //    while (!cancelWait.IsCancellationRequested)
+                //    {
+                //        Thread.Sleep(100);
+                //    }
+
+                //});
+                //t.Start();
+                //t.Wait(cancelWait.Token);
+            }
+            
+            //return result;
+        }
+
+        private async Task Handle(CancellationToken cancellationToken)
+        {
         }
     }
 }
