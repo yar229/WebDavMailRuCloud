317beb227b8b794a36051f13c35a4e157d96b034
YaR <yar229@yandex.ru>
2017-08-31 22:49:04 +0300

    dirty cookies save in progress (reverted from commit 597b14223ffa33a02b2250e4102ea6dc3a2ab77e)

--- WDMRC.Console/Program.cs
+++ WDMRC.Console/Program.cs
@@ -50,14 +50,6 @@ namespace YaR.WebDavMailRu
 
 
                     var cancellationTokenSource = new CancellationTokenSource();
-                    //cancellationTokenSource.Token.Register(Cloud.SaveCoockies);
-                    Console.CancelKeyPress += (sender, eventArgs) =>
-                    {
-                        Cloud.SaveCoockies();
-                        cancellationTokenSource.Cancel();
-                        eventArgs.Cancel = true;
-                    };
-
                     var httpListener = new HttpListener();
                     try
                     {
@@ -75,10 +67,8 @@ namespace YaR.WebDavMailRu
                     }
                     finally
                     {
-                        //cancellationTokenSource.Cancel();
+                        cancellationTokenSource.Cancel();
                         httpListener.Stop();
-
-                        //Cloud.SaveCoockies();
                     }
                     return 0;
 
@@ -145,10 +135,6 @@ namespace YaR.WebDavMailRu
             {
                 Logger.Error("Global exception", e);
             }
-            //finally
-            //{
-            //    Cloud.SaveCoockies();
-            //}
 
 
 
--- WebDavMailRuCloudStore/Cloud.cs
+++ WebDavMailRuCloudStore/Cloud.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.IO;
 using System.Net;
-using System.Runtime.Serialization.Formatters.Soap;
 using MailRuCloudApi;
 using MailRuCloudApi.Api;
 using NWebDav.Server.Http;
-using File = MailRuCloudApi.File;
 
 namespace YaR.WebDavMailRu.CloudStore
 {
@@ -57,49 +54,13 @@ namespace YaR.WebDavMailRu.CloudStore
                         Logger.Error($"Cannot load two-factor auth handler {TwoFactorHandlerName}");
                 }
 
-                CookieContainer cc = LoadCoockies(identity.Name);
-                cloud = new SplittedCloud(identity.Name, identity.Password, twoFaHandler, cc);
+                cloud = new SplittedCloud(identity.Name, identity.Password, twoFaHandler);
                 if (!CloudCache.TryAdd(key, cloud))
                     CloudCache.TryGetValue(key, out cloud);
             
 
                 return cloud;
             }
-        }
-
-        private static string _appdatapath {
-            get
-            {
-                string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "WDMRC\\Cookies");
-                if (!Directory.Exists(path)) Directory.CreateDirectory(path);
-                return path;
-            }
-        }
-            
-
-        public static void SaveCoockies()
-        {
-            foreach (var cloud in CloudCache.Values)
-            {
-
-                var formatter = new SoapFormatter();
-                string file = Path.Combine(_appdatapath, $"{cloud.CloudApi.Account.LoginName}.coockie.dat");
-
-                using (Stream s = System.IO.File.Create(file))
-                    formatter.Serialize(s, cloud.CloudApi.Account.Cookies);
-            }
-        }
-
-        private static CookieContainer LoadCoockies(string login)
-        {
-            var formatter = new SoapFormatter();
-            CookieContainer retrievedCookies = null;
-            string file = Path.Combine(_appdatapath, $"{login}.coockie.dat");
-            if (System.IO.File.Exists(file))
-                using (Stream s = System.IO.File.OpenRead(file))
-                    retrievedCookies = (CookieContainer)formatter.Deserialize(s);
-
-            return retrievedCookies;
         }
     }
 }
