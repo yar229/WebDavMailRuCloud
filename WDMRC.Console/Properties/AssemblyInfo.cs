a9a657103db93471e1d3e003ba5dcf4041c5483a
YaR <yar229@yandex.ru>
2017-09-12 12:50:58 +0300

    version bump

--- WDMRC.Console/Properties/AssemblyInfo.cs
+++ WDMRC.Console/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1.3")]
-[assembly: AssemblyFileVersion("1.6.1.3")]
+[assembly: AssemblyVersion("1.6.1.4")]
+[assembly: AssemblyFileVersion("1.6.1.4")]
 
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
