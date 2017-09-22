2f2dd1b9f01d5b147b732dddbd2704abf1ef6c70
YaR <yar229@yandex.ru>
2017-09-22 21:53:16 +0300

    version bump

--- WDMRC.Console/Properties/AssemblyInfo.cs
+++ WDMRC.Console/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.0.2")]
-[assembly: AssemblyFileVersion("1.7.0.1")]
+[assembly: AssemblyFileVersion("1.7.0.2")]
 
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
