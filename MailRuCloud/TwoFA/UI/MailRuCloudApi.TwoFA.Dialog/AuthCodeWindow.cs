22be9a4fa376d497a1c6ef2549fb0a66d169a07d
YaR <yar229@yandex.ru>
2017-10-26 17:36:41 +0300

    restore .NET45 WinForms TwoFA dialog

---
+++ MailRuCloud/TwoFA/UI/MailRuCloudApi.TwoFA.Dialog/AuthCodeWindow.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using YaR.MailRuCloud.Api;
+
+namespace YaR.MailRuCloud.TwoFA.UI
+{
+    public class AuthCodeWindow : ITwoFaHandler
+    {
+        public AuthCodeWindow()
+        { }
+
+        public string Get(string login, bool isAutoRelogin)
+        {
+            Application.EnableVisualStyles();
+
+            using (NotifyIcon notify = new NotifyIcon())
+            using (AuthCodeForm prompt = new AuthCodeForm())
+            {
+                var prompt1 = prompt;
+
+                notify.Visible = true;
+                notify.Icon = SystemIcons.Exclamation;
+                notify.BalloonTipTitle = "WebDAV Mail.Ru 2 Factor Auth";
+                notify.BalloonTipText = $"Auth code required for {login}";
+                notify.BalloonTipIcon = ToolTipIcon.Info;
+
+                notify.Click += (sender, args) =>
+                    prompt1?.Activate();
+                notify.BalloonTipClicked += (sender, args) =>
+                    prompt1?.Activate();
+
+                notify.ShowBalloonTip(30000);
+
+                var res = prompt.ShowAuthDialog(login, isAutoRelogin);
+
+                return res.AuthCode;
+            }
+        }
+    }
+}
