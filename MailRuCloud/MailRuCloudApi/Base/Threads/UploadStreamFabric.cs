cba0fee11c61c7c23c582e6f7086b8dd45ad5804
YaR <yar229@yandex.ru>
2017-11-19 15:12:57 +0300

    refact a bit

--- MailRuCloud/MailRuCloudApi/Base/Threads/UploadStreamFabric.cs
+++ MailRuCloud/MailRuCloudApi/Base/Threads/UploadStreamFabric.cs
@@ -16,36 +16,45 @@ namespace YaR.MailRuCloud.Api.Base.Threads
 
         public async Task<Stream> Create(File file, FileUploadedDelegate onUploaded = null, bool discardEncryption = false)
         {
-            var folder = await _cloud.GetItem(file.Path, MailRuCloud.ItemType.Folder) as Folder;
-            if (null == folder)
+            if (!(await _cloud.GetItem(file.Path, MailRuCloud.ItemType.Folder) is Folder folder))
                 throw new DirectoryNotFoundException(file.Path);
 
-            if (folder.CryptRequired && !discardEncryption)
-            {
-                var info = CryptoUtil.GetCryptoKeyAndSalt(_cloud.CloudApi.Account.Credentials.PasswordCrypt);
-                var xts = XtsAes256.Create(info.Key, info.IV);
-
-                file.ServiceInfo.CryptInfo = new CryptInfo
-                {
-                    PublicKey = new CryptoKeyInfo{Salt = info.Salt, IV = info.IV},
-                    AlignBytes = (uint) (file.Size % XTSWriteOnlyStream.BlockSize != 0 ? XTSWriteOnlyStream.BlockSize - file.Size % XTSWriteOnlyStream.BlockSize : 0)
-                };
-
-                var size = file.OriginalSize % XTSWriteOnlyStream.BlockSize == 0
-                    ? file.OriginalSize.DefaultValue
-                    : (file.OriginalSize / XTSWriteOnlyStream.BlockSize + 1) * XTSWriteOnlyStream.BlockSize;
-
-                var ustream = new SplittedUploadStream(file.FullPath, _cloud, size, false, file.ServiceInfo.CryptInfo);
-                if (onUploaded != null) ustream.FileUploaded += onUploaded;
-                var encustream = new XTSWriteOnlyStream(ustream, xts, XTSWriteOnlyStream.DefaultSectorSize);
-
-                return encustream;
-            }
-
-            var stream = new SplittedUploadStream(file.FullPath, _cloud, file.Size);
-            if (onUploaded != null) stream.FileUploaded += onUploaded;
+            var stream = folder.CryptRequired && !discardEncryption
+                ? GetCryptoStream(file, onUploaded)
+                : GetPlainStream(file, onUploaded);
 
             return stream;
+        }
+
+        private Stream GetPlainStream(File file, FileUploadedDelegate onUploaded)
+        {
+            var stream = new SplittedUploadStream(file.FullPath, _cloud, file.Size);
+            if (onUploaded != null) stream.FileUploaded += onUploaded;
+            return stream;
+        }
+
+        private Stream GetCryptoStream(File file, FileUploadedDelegate onUploaded)
+        {
+            var info = CryptoUtil.GetCryptoKeyAndSalt(_cloud.CloudApi.Account.Credentials.PasswordCrypt);
+            var xts = XtsAes256.Create(info.Key, info.IV);
+
+            file.ServiceInfo.CryptInfo = new CryptInfo
+            {
+                PublicKey = new CryptoKeyInfo {Salt = info.Salt, IV = info.IV},
+                AlignBytes = (uint) (file.Size % XTSWriteOnlyStream.BlockSize != 0
+                    ? XTSWriteOnlyStream.BlockSize - file.Size % XTSWriteOnlyStream.BlockSize
+                    : 0)
+            };
+
+            var size = file.OriginalSize % XTSWriteOnlyStream.BlockSize == 0
+                ? file.OriginalSize.DefaultValue
+                : (file.OriginalSize / XTSWriteOnlyStream.BlockSize + 1) * XTSWriteOnlyStream.BlockSize;
+
+            var ustream = new SplittedUploadStream(file.FullPath, _cloud, size, false, file.ServiceInfo.CryptInfo);
+            if (onUploaded != null) ustream.FileUploaded += onUploaded;
+            var encustream = new XTSWriteOnlyStream(ustream, xts, XTSWriteOnlyStream.DefaultSectorSize);
+
+            return encustream;
         }
     }
 }
