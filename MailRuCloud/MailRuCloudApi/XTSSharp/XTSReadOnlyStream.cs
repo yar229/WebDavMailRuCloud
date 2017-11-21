dffdb405b6ad201b29189b2a5aee882147ac5e71
YaR <yar229@yandex.ru>
2017-11-21 20:12:42 +0300

    #22 partial downloads seems working

--- MailRuCloud/MailRuCloudApi/XTSSharp/XTSReadOnlyStream.cs
+++ MailRuCloud/MailRuCloudApi/XTSSharp/XTSReadOnlyStream.cs
@@ -1,45 +1,107 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using YaR.WebDavMailRu.CloudStore.XTSSharp;
 
 namespace YaR.MailRuCloud.Api.XTSSharp
 {
     class XTSReadOnlyStream : XtsSectorStream
     {
-        private int _trimStart;
-        private readonly byte[] _tempBuffer;
+        private readonly Stream _baseStream;
+        private readonly XtsCryptoTransform _decryptor;
 
-        public XTSReadOnlyStream(Stream baseStream, Xts xts, int sectorSize, int trimStart, uint trimEnd) : base(baseStream, xts, sectorSize)
+        public XTSReadOnlyStream(Stream baseStream, Xts xts, int sectorSize, ulong startSector, int trimStart, uint trimEnd) : base(baseStream, xts, sectorSize)
         {
-            _trimStart = trimStart;
-
-            Length = baseStream.Length - _trimStart - trimEnd;
-
+            _baseStream = baseStream;
             _tempBuffer = new byte[SectorSize];
+            _decryptor = xts.CreateDecryptor();
+            _currentSector = startSector;
+            Length = baseStream.Length - trimStart - trimEnd;
+
+            if (trimStart > 0)
+            {
+                int read = ReadExactly(_baseStream, _tempBuffer, sectorSize);
+                _decryptor.TransformBlock(_tempBuffer, 0, SectorSize, _tempBuffer, 0, _currentSector);
+                _tempBufferCount = read;
+                _tempBufferPos = trimStart;
+                _currentSector++;
+            }
         }
 
         private long _position;
 
+
+        private readonly byte[] _tempBuffer;
+        private int _tempBufferPos;
+        private int _tempBufferCount;
+
+        private ulong _currentSector;
+
+
         public override int Read(byte[] buffer, int offset, int count)
         {
-            int toread, read, totalRead = 0;
-            while (
-                offset < count &&
-                (toread = Math.Max(Math.Min(SectorSize, count - offset), SectorSize)) > 0 &&
-                (read = base.Read(_tempBuffer, 0, toread)) != 0)
-            {
-                _position += read;
-                Array.Copy(_tempBuffer, _trimStart, buffer, offset + _trimStart, read - _trimStart);
-                offset += read - _trimStart;
-                totalRead += read - _trimStart;
+            int totalread = 0;
 
-                if (_trimStart > 0) _trimStart = 0;
+            if (_tempBufferCount > 0)
+            {
+                int tbtocopy = Math.Min(count - offset, _tempBufferCount - _tempBufferPos);
+                if (tbtocopy > 0)
+                {
+                    Array.Copy(_tempBuffer, _tempBufferPos, buffer, offset, tbtocopy);
+                    offset += tbtocopy;
+                    _tempBufferPos += tbtocopy;
+                    totalread += tbtocopy;
+                    _position += tbtocopy;
+                    if (_tempBufferPos == _tempBufferCount)
+                    {
+                        _tempBufferPos = 0;
+                        _tempBufferCount = 0;
+                    }
+                }
             }
 
-            if (_position > Length)
-                return totalRead - (int)(_position - Length);
+            while (offset < count && _position < Length)
+            {
+                int read = ReadExactly(_baseStream, _tempBuffer, SectorSize);
+                int transformed = _decryptor.TransformBlock(_tempBuffer, 0, SectorSize, _tempBuffer, 0, _currentSector);
 
-            return totalRead;
+                _currentSector++;
+                _tempBufferCount = read;
+                _tempBufferPos = 0;
+
+                int tocopy = Math.Min(count - offset, read);
+
+                Array.Copy(_tempBuffer, _tempBufferPos, buffer, offset, tocopy);
+                _tempBufferPos += tocopy;
+                offset += tocopy;
+                totalread += tocopy;
+                _position += tocopy;
+
+                if (_tempBufferPos == _tempBufferCount)
+                {
+                    _tempBufferPos = 0;
+                    _tempBufferCount = 0;
+                }
+            }
+
+            return totalread;
+        }
+
+
+        private static int ReadExactly(Stream stream, byte[] buffer, int count)
+        {
+            int offset = 0;
+            int totalread = 0;
+            while (offset < count)
+            {
+                int read = stream.Read(buffer, offset, count - offset);
+                totalread += read;
+                if (read == 0)
+                    return totalread;
+                offset += read;
+            }
+            System.Diagnostics.Debug.Assert(offset == count);
+            return totalread;
         }
 
         public override long Length { get; }
