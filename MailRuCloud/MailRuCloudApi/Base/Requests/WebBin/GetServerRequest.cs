f2978e5db8803f1b60d28ffdfb2aeed1bd1eaf84
YaR <yar229@yandex.ru>
2018-01-18 12:48:03 +0300

    fixed linked file download fails

---
+++ MailRuCloud/MailRuCloudApi/Base/Requests/WebBin/GetServerRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace YaR.MailRuCloud.Api.Base.Requests.WebBin
+{
+    internal class GetServerRequest : ServerRequest
+    {
+        public GetServerRequest(HttpCommonSettings settings) : base(settings)
+        {
+        }
+        
+        protected override string RelationalUri => "https://dispatcher.cloud.mail.ru/d";
+    }
+}
