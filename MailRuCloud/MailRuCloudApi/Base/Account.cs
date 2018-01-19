fbb4cadb226ac28d8731237b7bff4a2ab308fb28
YaR <yar229@yandex.ru>
2018-01-19 16:25:36 +0300

    request RepoFabric

--- MailRuCloud/MailRuCloudApi/Base/Account.cs
+++ MailRuCloud/MailRuCloudApi/Base/Account.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Net;
+﻿using System.Net;
 using System.Threading.Tasks;
 using YaR.MailRuCloud.Api.Base.Repos;
 using YaR.MailRuCloud.Api.Base.Requests.Types;
@@ -27,27 +26,11 @@ namespace YaR.MailRuCloud.Api.Base
             WebRequest.DefaultWebProxy.Credentials = CredentialCache.DefaultCredentials;
             Proxy = WebRequest.DefaultWebProxy;
 
-            var twoFaHandler1 = settings.TwoFaHandler;
-            if (twoFaHandler1 != null)
-                AuthCodeRequiredEvent += twoFaHandler1.Get;
-
-            //TODO: make repo fabric
-            RequestRepo = Protocol.WebM1Bin == settings.Protocol
-                ? (IRequestRepo)new WebM1RequestRepo(Proxy, Credentials, OnAuthCodeRequired)
-                : Protocol.WebV2 == settings.Protocol
-                    ? new WebV2RequestRepo(Proxy, Credentials, OnAuthCodeRequired)
-                    : throw new Exception("Unknown protocol");
-
-            if (!string.IsNullOrWhiteSpace(settings.UserAgent))
-                RequestRepo.HttpSettings.UserAgent = settings.UserAgent;
+            RequestRepo = new RepoFabric(settings, credentials, Proxy)
+                .Create();
         }
 
         internal IRequestRepo RequestRepo { get; }
-                                            //   ??
-                                            //(_requestRepo = new MobileRequestRepo(_cloudApi.Account.Proxy, _cloudApi.Account.Credentials)); 
-                                            //(_requestRepo = new WebV2RequestRepo(_cloudApi.Account.Proxy, new WebAuth(_cloudApi.Account.Proxy, _cloudApi.Account.Credentials,OnAuthCodeRequired)));
-                                            //------(_requestRepo = new WebM1RequestRepo(Proxy, Credentials, OnAuthCodeRequired));
-                                            //MixedRepo(_cloudApi));
 
         /// <summary>
         /// Gets connection proxy.
@@ -80,18 +63,10 @@ namespace YaR.MailRuCloud.Api.Base
         /// <returns>True or false result operation.</returns>
         public async Task<bool> LoginAsync()
         {
-            //await RequestRepo.Login(OnAuthCodeRequired);
-
             Info = await RequestRepo.AccountInfo();
-
             return true;
         }
 
-        public event AuthCodeRequiredDelegate AuthCodeRequiredEvent;
-        protected virtual string OnAuthCodeRequired(string login, bool isAutoRelogin)
-        {
-            return AuthCodeRequiredEvent?.Invoke(login, isAutoRelogin);
-        }
     }
 
     public delegate string AuthCodeRequiredDelegate(string login, bool isAutoRelogin);
---
+++ MailRuCloud/MailRuCloudApi/Base/Repos/RepoFabric.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+
+namespace YaR.MailRuCloud.Api.Base.Repos
+{
+    class RepoFabric
+    {
+        private readonly CloudSettings _settings;
+        private readonly Credentials _credentials;
+        private readonly IWebProxy _proxy;
+
+        public RepoFabric(CloudSettings settings, Credentials credentials, IWebProxy proxy)
+        {
+            _settings = settings;
+            _credentials = credentials;
+            _proxy = proxy;
+        }
+
+        public IRequestRepo Create()
+        {
+            string TwoFaHandler(string login, bool isAutoRelogin) => _settings.TwoFaHandler?.Get(login, isAutoRelogin);
+
+            IRequestRepo repo;
+            switch (_settings.Protocol)
+            {
+                case Protocol.WebM1Bin:
+                    repo = new WebM1RequestRepo(_proxy, _credentials, TwoFaHandler);
+                    break;
+                case Protocol.WebV2:
+                    repo = new WebV2RequestRepo(_proxy, _credentials, TwoFaHandler);
+                    break;
+                default:
+                    throw new Exception("Unknown protocol");
+            }
+
+            if (!string.IsNullOrWhiteSpace(_settings.UserAgent))
+                repo.HttpSettings.UserAgent = _settings.UserAgent;
+
+            return repo;
+        }
+
+        //   ??
+        //(_requestRepo = new MobileRequestRepo(_cloudApi.Account.Proxy, _cloudApi.Account.Credentials)); 
+        //(_requestRepo = new WebV2RequestRepo(_cloudApi.Account.Proxy, new WebAuth(_cloudApi.Account.Proxy, _cloudApi.Account.Credentials,OnAuthCodeRequired)));
+        //------(_requestRepo = new WebM1RequestRepo(Proxy, Credentials, OnAuthCodeRequired));
+        //MixedRepo(_cloudApi));
+
+    }
+}
