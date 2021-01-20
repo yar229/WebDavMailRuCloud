1178bf340f89c158fcdeae80afb5fadeb94f3554
YaR <yar229@yandex.ru>
2021-01-20 20:31:19 +0300

    #217 SmtpAsyncAppender for log4net

---
+++ MailRuCloud/MailRuCloudApi/SmtpAsyncAppender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Appender;
+
+namespace YaR.Clouds
+{
+    public class SmtpAsyncAppender : SmtpAppender
+    {
+        protected override void SendEmail(string messageBody)
+        {
+            Task.Run(() =>
+            {
+                try
+                {
+                    base.SendEmail(messageBody);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            });
+        }
+    }
+}
